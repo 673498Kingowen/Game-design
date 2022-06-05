7642106c13f6b6086b93246c7eb24dccd6781d0f
Kingowen001 <106926034+Kingowen001@users.noreply.github.com>
2022-06-05 20:08:55 +0200

    Add files via upload

---
+++ Header_ToDo.h
@@ -0,0 +1,24 @@
+#ifndef TODO
+#define TODO
+
+
+using std::string;
+using std::int16_t;
+
+static int positionInList = 0;
+static int listSize = 0;
+
+struct MyToDo
+{
+	string description;
+	int priority;
+	string dueDate;
+
+
+
+};
+
+
+
+
+#endif 
